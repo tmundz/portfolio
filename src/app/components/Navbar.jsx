b4d1867058969d0c775a5dcaf32bf4918ca2a48b
Travis <mundyt4@mymacewan.ca>
2023-11-03 16:40:55 -0600

    Started working on Navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href={"/"} className="text-5xl text-white font-semibold">LOGO</Link>
+        <div className="menu"></div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
