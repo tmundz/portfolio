6c501b9177f6e3329cf33b6e99131f6eeb83ac70
Travis Mundy <mundyt4@mymacewan.ca>
2023-11-04 01:34:30 -0600

    Finished nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 //import Image from 'next/image'
 import HeroSection from './components/HeroSection';
-
+import Navbar from './components/Navbar';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#090915] container mx-auto px-12 p4">
+      <Navbar />
+      <div class="container mt-24 mx-auto"></div>
       <HeroSection />
     </main>
   );
