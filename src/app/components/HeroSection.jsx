74685143ff50f0c7b411cb8bf27f3c884ef72728
Travis <mundyt4@mymacewan.ca>
2023-11-08 21:59:47 -0700

    make small fixes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,17 +9,19 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm{" "} </span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm{" "}
+            </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                'Travis Mundy',
+                "Travis Mundy",
                 2000,
-                'Software Dev',
+                "Software Dev",
                 2000,
-                'Systems Dev',
+                "Systems Dev",
                 2000,
-                'Embedded Dev',
+                "Embedded Dev",
                 2000,
               ]}
               wrapper="span"
@@ -35,10 +37,14 @@ const HeroSection = () => {
           </p>
           <div>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-600 via-purple-500 to-red-50 hover:bg-slate-800 text-white">
-             <span className="block bg-[#090915] hover:bg-[#4f2B43] rounded-full px-5 py-2">Hire Me</span>
+              <span className="block bg-[#090915] hover:bg-[#4f2B43] rounded-full px-5 py-2">
+                Hire Me
+              </span>
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-600 via-purple-500 to-red-50 hover:bg-slate-800 text-white mt-3">
-              <span className="block bg-[#090915] hover:bg-[#4f2B43] rounded-full px-5 py-2">Download CV </span>
+              <span className="block bg-[#090915] hover:bg-[#4f2B43] rounded-full px-5 py-2">
+                Download CV{" "}
+              </span>
             </button>
           </div>
         </div>
@@ -54,7 +60,8 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-    </section>);
+    </section>
+  );
 };
 
 export default HeroSection;
