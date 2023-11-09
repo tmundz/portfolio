8939a6afa77a1ca063bb34b75265d1327fb66153
Travis <mundyt4@mymacewan.ca>
2023-11-08 21:52:38 -0700

    Added project sections
    Will need further edits

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 import AboutSection from './components/AboutSection';
+import ProjectSection from './components/ProjectSection';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#090915] container mx-auto px-12 p4">
@@ -9,6 +10,7 @@ export default function Home() {
       <div class="container mt-24 mx-auto"></div>
       <HeroSection />
       <AboutSection />
+      <ProjectSection />
     </main>
   );
 }
