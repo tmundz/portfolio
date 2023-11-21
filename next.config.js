ba3ad0699a47cdf46be1e1077c740cd234dfc799
Travis <mundyt4@mymacewan.ca>
2023-11-20 18:19:13 -0700

    edit to fix images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export'}
+const repo = "portfolio";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+const nextConfig = {
+  trailingSlash: true,
+
+  output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig
 
