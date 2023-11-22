c96b85c86252ab279be84e3e32d81ce6368a5f2b
Travis <mundyt4@mymacewan.ca>
2023-11-21 18:16:53 -0700

    Attempt navbar fix

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 const NavLink = ({ href, title }) => {
   return (
     <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-    >
+      href={href}>
+      <a className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
+    
       {title}
+      </a>
     </Link>
   );
 };
