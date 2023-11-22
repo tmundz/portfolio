389b20ae435916d037a2a395668c28dc955eb10b
Travis <mundyt4@mymacewan.ca>
2023-11-21 18:11:10 -0700

    update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -74,7 +74,7 @@ const AboutSection = () => {
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
                 <Image src="images/about-image.png" alt="" width={500} height={500} />
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-                    <h2 className="text-xl font-bold text-white mb-4">About Me</h2>
+                    <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">
                     I am a software developer interested in embedded systems, full-stack development, and systems programming. 
                     My expertise revolves around crafting efficient low-level programs, 
